70a2e4cc3a9af107253024ab070153e5c43a3015
Damien <DamienB2474@protonmail.com>
2023-01-29 22:53:25 -0500

    Basic Infrastructure

---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./home.scss";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Valencia Tech Club</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
